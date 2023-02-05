0ea09ba159ddcd49c7d2f24b6b165d1b50dfba0b
Jonathan Gucciardi <jonmgucciardi@gmail.com>
2023-02-05 14:37:31 -0600

    cleaned up git

---
+++ Interfaces/IHeaderData.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace ComponentSandbox.Interfaces;
+
+/*this will be referenced like:
+     public IHeaderData? HeaderData { get; private set; }
+     
+     <div>
+        @HeaderData?.ChildContent
+    </div>
+     
+*/
+public interface IHeaderData
+{
+
+    RenderFragment ChildContent { get; }
+}
---
+++ Templates/CardHeaderTemplate.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace ComponentSandbox.Templates;
+
+public class CardHeaderTemplate : ComponentBase
+{
+    [Parameter]
+    public string? Data { get; set; }
+
+    [Parameter] public RenderFragment<string>? ChildContent { get; set; } = default;
+    
+    protected override void OnInitialized()
+    {
+        
+        base.OnInitialized();
+    }
+}
